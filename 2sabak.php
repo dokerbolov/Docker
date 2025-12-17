2d342e1b561dc42985be09bc6436b781d3fd31ad
dokerbolov <proface19@gmail.com>
2025-12-17 22:00:01 +0500

    2nd lesson

---
+++ 2sabak.php
@@ -0,0 +1,86 @@
+<?php
+
+
+# Бірінші мысал
+//echo "Жасынызды еңгізініз: ";
+//$age = trim(fgets(STDIN));
+//
+//if ($age >= 18) {
+//    echo "Сіз ересексіз.";
+//} else {
+//    echo "Сіз кәмелетке толмағансыз.";
+//}
+
+# Екінші мысал
+//echo "1-ші санды еңгізініз: ";
+//$num1 = trim(fgets(STDIN));
+//echo "2-ші санды еңгізініз: ";
+//$num2 = trim(fgets(STDIN));
+//
+//if($num1 == $num2) {
+//    echo "Екі сан бір-біріне тен";
+//} elseif($num1 > $num2) {
+//    echo "Бірінші сан, екінші саннан үлкен";
+//} else {
+//    echo "Екінші сан, бірінші саннан үлкен";
+//}
+
+#Үшінші мысал
+//echo "Санды еңгізініз: ";
+//$num = trim(fgets(STDIN));
+//echo $num * 2;
+
+#Төртінші мысал
+//echo "Жасағыныз келген опперацияны танданыз 1-> +; 2-> -; 3-> *; 4-> /: ";
+//
+//$operation = trim(fgets(STDIN));
+//
+//switch ($operation) {
+//    case 1:
+//        echo "1-ші санды еңгізініз: ";
+//        $num1 = trim(fgets(STDIN));
+//        echo "2-ші санды еңгізініз: ";
+//        $num2 = trim(fgets(STDIN));
+//        $total = $num1 + $num2;
+//        echo "Сандардың қосындысы: " . $total;
+//        break;
+//    case 2:
+//        echo "1-ші санды еңгізініз: ";
+//        $num1 = trim(fgets(STDIN));
+//        echo "2-ші санды еңгізініз: ";
+//        $num2 = trim(fgets(STDIN));
+//        $total = $num1 - $num2;
+//        echo "Сандардың айырмасы: " . $total;
+//        break;
+//    case 3:
+//        echo "1-ші санды еңгізініз: ";
+//        $num1 = trim(fgets(STDIN));
+//        echo "2-ші санды еңгізініз: ";
+//        $num2 = trim(fgets(STDIN));
+//        $total = $num1 * $num2;
+//        echo "Сандардың кобейтіндісі: " . $total;
+//        break;
+//    case 4:
+//        echo "1-ші санды еңгізініз: ";
+//        $num1 = trim(fgets(STDIN));
+//        echo "2-ші санды еңгізініз: ";
+//        $num2 = trim(fgets(STDIN));
+//        $total = $num1 / $num2;
+//        echo "Сандардың бөліндісі: " . $total;
+//        break;
+//    default:
+//        echo "Сіз дұрыс емес операцияны тандадыңыз!";
+//        break;
+//}
+
+#Бесінші мысал "Тік бұрышты ұшбұрыштың гипотенузасын анықтау";
+
+//echo "1-ші санды еңгізініз: ";
+//$num1 = trim(fgets(STDIN));
+//echo "2-ші санды еңгізініз: ";
+//$num2 = trim(fgets(STDIN));
+//
+//$num3 = sqrt(($num1 * $num1) + ($num2 * $num2));
+//
+//echo $num3
+?>
